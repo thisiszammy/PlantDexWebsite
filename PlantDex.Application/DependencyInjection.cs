d7cc63d534b6209514724e3c223db603b4783c6e
mjzamoras <mjzamoras@gmail.com>
2020-10-17 02:49:53 +0800

    Initial Dependency Installation

---
+++ PlantDex.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace PlantDex.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+        {
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
