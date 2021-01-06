9a234aeefbf6a98ae1ee890451185bfe2d760cc8
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-07 04:57:55 +0800

    -I IDateTimeService

---
+++ PlantDex.Api/Services/DateTimeService.cs
@@ -0,0 +1,31 @@
+﻿using PlantDex.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlantDex.Api.Services
+{
+    public class DateTimeService : IDateTimeService
+    {
+        public DateTime format(string dateTime, string format, string timezone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime format(DateTime original, string timezone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime getCurrentDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setStandardFormat(string format)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
