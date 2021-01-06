bd15d8d9b77ca9e396b5f2c468acc719c9e722ff
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-07 06:38:03 +0800

    IAuthentication DTO Setup

---
+++ PlantDex.Api/Services/UserManagementService.cs
@@ -0,0 +1,30 @@
+﻿using PlantDex.Api.ViewModel;
+using PlantDex.Application.DataTransferObjects.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PlantDex.Infrastructure.Persistence;
+using PlantDex.Application.Services;
+
+namespace PlantDex.Api.Services
+{
+    public class UserManagementService : IUserManagementService
+    {
+
+
+        public Task<UserManagementResponse> LoginAsync(LoginViewModel loginViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserManagementResponse> RegisterAsync(RegisterViewModel registerViewModel)
+        {
+            if (registerViewModel == null)
+                throw new Exception("Register model is null");
+
+
+            return null;
+        }
+    }
+}
