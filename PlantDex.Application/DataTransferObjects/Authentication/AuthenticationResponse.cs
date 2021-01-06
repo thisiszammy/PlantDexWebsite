ac28347dd706eb6235d41493e5105254866bf418
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-07 06:17:35 +0800

    IAuthentication DTO Setup

---
+++ PlantDex.Application/DataTransferObjects/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantDex.Application.DataTransferObjects.Authentication
+{
+    public class AuthenticationResponse
+    {
+        public string Message { get; set; }
+        public bool IsSuccessful { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
---
+++ PlantDex.Application/DataTransferObjects/Authentication/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PlantDex.Application.DataTransferObjects.Authentication
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public strsing Password { get; set; }
+    }
+}
