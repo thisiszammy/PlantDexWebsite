19dbb7c1aee28766ec5a35abbe04285826bdf104
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-07 06:17:35 +0800

    IAuthentication DTO Setup

---
+++ PlantDex.Application/Services/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using PlantDex.Api.ViewModel;
+using PlantDex.Application.DataTransferObjects.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantDex.Application.Services
+{
+    public interface IAuthenticationService
+    {
+        public Task<AuthenticationResponse> Register(RegisterViewModel registerViewModel);
+        public Task<AuthenticationResponse> Login(LoginViewModel loginViewModel);
+    }
+}
