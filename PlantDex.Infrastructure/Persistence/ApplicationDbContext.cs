fd71e16fff30ea03483f4b70cc880c22e5e4b63e
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-07 05:07:56 +0800

    -I IDateTimeService

--- PlantDex.Infrastructure/Persistence/ApplicationDbContext.cs
+++ PlantDex.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -16,34 +16,38 @@ namespace PlantDex.Infrastructure.Persistence
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>,IApplicationDbContext
     {
         private readonly ICurrentUserService currentUserService;
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, ICurrentUserService currentUserService)
+        private readonly IDateTimeService dateTimeService;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, ICurrentUserService currentUserService,
+            IDateTimeService dateTimeService)
             : base(options)
         {
             this.currentUserService = currentUserService;
+            this.dateTimeService = dateTimeService;
         }
 
         public DbSet<Plant> Plants { get; set; }
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
         {
-            //foreach (var item in ChangeTracker.Entries<AuditableEntity>())
-            //{
-            //    switch (item.State)
-            //    {
-            //        case EntityState.Added:
-            //            {
-            //                item.Entity.CreatedBy = currentUserService.currentUserId;
-            //                item.Entity.CreatedOn = dateTimeService.currentDateTime;
-            //                break;
-            //            }
-            //        case EntityState.Modified:
-            //            {
-            //                item.Entity.LastModifiedBy = currentUserService.currentUserId;
-            //                item.Entity.LastModifiedOn = dateTimeService.currentDateTime;
-            //                break;
-            //            }
-            //    }
-            //}
+            foreach (var item in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (item.State)
+                {
+                    case EntityState.Added:
+                        {
+                            item.Entity.CreatedBy = currentUserService.currentUser;
+                            item.Entity.CreatedOn = dateTimeService.getCurrentDate();
+                            break;
+                        }
+                    case EntityState.Modified:
+                        {
+                            item.Entity.LastModifiedBy = currentUserService.currentUser;
+                            item.Entity.LastModifiedOn = dateTimeService.getCurrentDate();
+                            break;
+                        }
+                }
+            }
+
             return base.SaveChangesAsync(cancellationToken);
 
         }
