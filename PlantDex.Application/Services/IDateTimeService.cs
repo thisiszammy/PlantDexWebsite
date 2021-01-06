bbbdf49a462108244110fc6b8ddef25bb59a648d
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-07 04:57:55 +0800

    -I IDateTimeService

---
+++ PlantDex.Application/Services/IDateTimeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantDex.Application.Services
+{
+    public interface IDateTimeService
+    {
+        public DateTime getCurrentDate();
+        public DateTime format(string dateTime, string format, string timezone);
+        public DateTime format(DateTime original, string timezone);
+        public void setStandardFormat(string format);
+    }
+}
