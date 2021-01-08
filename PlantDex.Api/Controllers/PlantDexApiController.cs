ae49dcd13cad32bbe0ed7d0598e993b91b0f4e3a
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-08 15:00:18 +0800

    GetPlantByScientificNameQuery

--- PlantDex.Api/Controllers/PlantDexApiController.cs
+++ PlantDex.Api/Controllers/PlantDexApiController.cs
@@ -4,12 +4,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PlantDex.Application.Common.Plants.Commands;
+using PlantDex.Application.DTO.PlantsManagement;
 
 namespace PlantDex.Api.Controllers
 {
+    [Route("api/plants")]
     [ApiController]
     public class PlantDexApiController : ControllerBase
     {
+        [HttpPost("add")]
+        public async Task<IActionResult> AddPlant([FromBody] AddPlantCommand addPlantCommand)
+        {
 
+            if (ModelState.IsValid)
+            {
+
+                if (addPlantCommand == null)
+                    return BadRequest(new PlantsManagementResponse
+                    {
+                        isSuccessful = false,
+                        message = "Request body is empty"
+                    });
+
+
+
+
+            }
+            return BadRequest(new PlantsManagementResponse
+            {
+                isSuccessful = false,
+                message = "An unknown error has occurred"
+            });
+        }
     }
 }
