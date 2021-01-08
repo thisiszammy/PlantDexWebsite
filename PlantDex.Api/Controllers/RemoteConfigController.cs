6a985d108e759efdc762a62f04ca9eb617883c13
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-08 15:47:48 +0800

    RemoteConfigurationController

---
+++ PlantDex.Api/Controllers/RemoteConfigController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlantDex.Application;
+using PlantDex.Application.DTO.RemoteConfiguration;
+
+namespace PlantDex.Api.Controllers
+{
+    [Route("api/remote-config")]
+    [ApiController]
+    public class RemoteConfigController : ControllerBase
+    {
+        private readonly ApplicationConstants applicationConstants;
+
+        public RemoteConfigController(ApplicationConstants applicationConstants)
+        {
+            this.applicationConstants = applicationConstants;
+        }
+
+        [HttpGet("/version")]
+        public IActionResult GetConfig()
+        {
+            return Ok(new RemoteConfigResponse
+            {
+                isSuccessful = true,
+                message = "Successfully retrieved application configuration",
+                version = applicationConstants.version
+            });
+        }
+    }
+}
---
+++ PlantDex.Application/DTO/RemoteConfiguration/RemoteConfigResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantDex.Application.DTO.RemoteConfiguration
+{
+    public class RemoteConfigResponse
+    {
+        public string message { get; set; }
+        public bool isSuccessful { get; set; }
+        public string version { get; set; }
+    }
+}
