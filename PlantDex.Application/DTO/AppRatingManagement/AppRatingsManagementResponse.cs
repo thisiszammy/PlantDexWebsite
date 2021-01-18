f27b47f5cab0e36bd688b02dd6ae1cae0ef55dbb
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-18 09:12:13 +0800

    AppRatingFunctionality

---
+++ PlantDex.Application/DTO/AppRatingManagement/AppRatingsManagementResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantDex.Application.DTO.AppRatingManagement
+{
+    public class AppRatingsManagementResponse
+
+    {
+        public string message { get; set; }
+        public bool isSuccessful { get; set; }
+    }
+}
