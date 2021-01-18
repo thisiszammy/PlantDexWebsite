6aef0341f755ccad67f883c2b414ac0da0aed91b
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-18 08:23:05 +0800

    Added CustomerSupport Essentials

---
+++ PlantDex.Domain/Entities/SubmittedComplaint.cs
@@ -0,0 +1,17 @@
+﻿using PlantDex.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantDex.Domain.Entities
+{
+    public class SubmittedComplaint : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string userId { get; set; }
+        public string appVersion { get; set; }
+        public string phoneVersion { get; set; }
+        public string remarks { get; set; }
+
+    }
+}
