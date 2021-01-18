ea167f3a296bacc1ec11c9fe5fc2cfc306afe5d2
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-18 13:32:35 +0800

    Completed ComplaintFilingFunctionality

--- PlantDex.Api/Controllers/CustomerSupportController.cs
+++ PlantDex.Api/Controllers/CustomerSupportController.cs
@@ -7,7 +7,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PlantDex.Application;
 using PlantDex.Application.Common.AppRatings.Commands;
+using PlantDex.Application.Common.SubmittedComplaints.Commands;
 using PlantDex.Application.DTO.AppRatingManagement;
+using PlantDex.Application.DTO.ComplaintsManagement;
 using PlantDex.Infrastructure.Persistence;
 
 namespace PlantDex.Api.Controllers
@@ -21,10 +23,12 @@ namespace PlantDex.Api.Controllers
         private readonly ApplicationSecrets applicationSecrets;
 
         public CustomerSupportController(IMediator mediator,
-            IUserManagementService userManagementService)
+            IUserManagementService userManagementService,
+            ApplicationSecrets applicationSecrets)
         {
             this.mediator = mediator;
             this.userManagementService = userManagementService;
+            this.applicationSecrets = applicationSecrets;
         }
 
         [HttpPost("rating/submit")]
@@ -67,6 +71,47 @@ namespace PlantDex.Api.Controllers
                 isSuccessful = false,
                 message = "An error has occurred"
             });
+        }
+
+        [HttpPost("complaints/file")]
+        public async Task<IActionResult> PostComplaints([FromBody] AddSubmittedComplaintCommand addSubmittedComplaintCommand)
+        {
+            string accessKey = Request.Headers["Authorization"].ToString();
+
+            if (accessKey.Trim().Length < 1 || applicationSecrets.authAccess != accessKey)
+                return Unauthorized(new AppRatingsManagementResponse
+                {
+                    isSuccessful = false,
+                    message = "Invalid Access Key"
+                });
+
+            if (ModelState.IsValid)
+            {
+                if (addSubmittedComplaintCommand == null)
+                    return BadRequest(new ComplaintsManagementResponse()
+                    {
+                        isSuccessful = false,
+                        message = "Invalid Parameters"
+                    });
+
+                if (!(await userManagementService.IsValidUserId(addSubmittedComplaintCommand.userId)))
+                    return BadRequest(new ComplaintsManagementResponse()
+                    {
+                        isSuccessful = false,
+                        message = "Invalid User ID"
+                    });
+
+                var taskAddAppRating = await mediator.Send(addSubmittedComplaintCommand);
+
+                return Ok(taskAddAppRating);
+            }
+
+            return BadRequest(new ComplaintsManagementResponse()
+            {
+                isSuccessful = false,
+                message = "An error has occurred"
+            });
+
         }
     }
 }
