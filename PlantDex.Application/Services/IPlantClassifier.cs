bbe021ceb0c7fe1fabf7d59b4069c5d85d1e7015
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-20 00:47:11 +0800

    Image Classifier Logic Setup

---
+++ PlantDex.Application/Services/IPlantClassifier.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlantDex.Application.Services
+{
+    public interface IPlantClassifier
+    {
+        public List<string> classifyImage(string filePath);
+    }
+}
