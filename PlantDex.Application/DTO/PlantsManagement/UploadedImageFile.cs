f423d31f4152972f53f84671e66b2accfb6f099a
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-21 04:28:26 +0800

    Added UploadedImageFile class

---
+++ PlantDex.Application/DTO/PlantsManagement/UploadedImageFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PlantDex.Application.DTO.PlantsManagement
+{
+    public class UploadedImageFile
+    {
+        public string fileName { get; set;}
+        public byte[] fileData { get; set;}
+
+        public UploadedImageFile(string filePath)
+        {
+            this.fileName = Path.GetFileName(Normalize(filePath));
+            this.fileData = File.ReadAllBytes(filePath);
+        }
+
+        private string Normalize(string input)
+        {
+            return new string(input
+              .Normalize(System.Text.NormalizationForm.FormD)
+              .Replace(" ", string.Empty)
+              .ToCharArray()
+              .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+              .ToArray());
+        }
+    }
+}
